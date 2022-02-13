bae3a64342058e23b8d2243db32245229a4057ec
Daniil <erema03302028@yandex.ru>
2022-02-14 00:33:30 +0300

    fix in UE

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -1,109 +1,85 @@
 ﻿#include <iostream>
+#include "Vector.h"
 
-typedef int Elem;
-
-struct Node
+UVector::UVector()
 {
-    Node* Next; 
-    Node* Prev; 
-    Elem Value;
-};
+    FNode* Array[1];
+    Capacity = 1;
+}
 
-class Vector {
-public:
-  class Iterator {
-  public:
-        Elem& operator*(); // reference to the element
-        bool operator==(const Iterator& p) const; // compare positions
-        bool operator!=(const Iterator& p) const;
-        Iterator& operator++(); // move to next position
-        Iterator& operator−−(); // move to previous position
-  };
-public:
-
-    Vector() // constructor
+UVector::UVector(const UVector& other) // copy ctor
+{
+    if (this != &other) 
     {
-        Node* Array[1];
-        Capacity = 1;
-    }
-
-    Vector(const Vector& other) // copy ctor
-    {
-        if (this != &other) 
+        delete[] Array;
+        size_t Array = other.Capacity;
+        for (size_t i = 0; i < other.SizeVector; i++)
         {
-            delete[] Array;
-            Array = other.Capacity;
-            for (size_t i = 0; i < other.SizeVector; i++)
-            {
-                Array[i] = other.Array[i];
-            }
-            SizeVector = other.SizeVector;
-            Capacity = other.Capacity;
-        }
-    }
-
-    Vector& operator=(const Vector& other) // copy assignment
-    {
-        if (this != &other)
-        {
-            delete[] Array;
-        }
-		Array = other.Capacity;
-        for (size_t i = 0; i < other.SizeVector; ++i)
-        {
-            Array[i] = other.Array[i];
+            Array[i] = other.SizeVector[i];
         }
         SizeVector = other.SizeVector;
         Capacity = other.Capacity;
-		return *this;
     }
+}
 
-    ~Vector() // destructor
+std::vector
+
+UVector::UVector& operator=(const UVector& other) // copy assignment
+{
+    if (this != &other)
     {
         delete[] Array;
     }
-
-    bool Empty() const // is vector empty?
+	Array = other.Capacity;
+    for (size_t i = 0; i < other.SizeVector; ++i)
     {
-        return SizeVector == 0;
+        Array[i] = other.Array[i];
     }
-
-    void Erase(size_t index) // remove element at index
-    {
-        for (size_t i = index + 1; i < SizeVector; ++i)
-        {
-            Array[i - 1] = Array[i];
-        }
-        --SizeVector;
-    }
-
-    Elem& operator[ ](size_t index) // element at index
-    {
-        return Array[index];
-    }
-
-    const Elem& operator[ ](size_t index) const // element at index
-    {
-        return Array[index];
-    }
-
-
-
-    Iterator begin() const; // start position
-    Iterator end() const // past the end position
-    int Count() const; // number of elements
-   
-    Elem& At(size_t i); // element at index
-    const Elem& At(size_t i) const; // element at index
-    
-    void Insert(size_t i, const Elem& e); // insert element at index
-    void Reserve(size_t n); // reserve at least N spots
-
-private:
-    Node* Array;
-    size_t SizeVector{};
-    size_t Capacity{};
+    SizeVector = other.SizeVector;
+    Capacity = other.Capacity;
+	return *this;
 }
+
+UVector::~UVector() // destructor
+{
+    delete[] Array;
+}
+
+bool UVector::Empty() const // is vector empty?
+{
+    return SizeVector == 0;
+}
+
+void UVector::Erase(size_t index) // remove element at index
+{
+    for (size_t i = index + 1; i < SizeVector; ++i)
+    {
+        Array[i - 1] = Array[i];
+    }
+    --SizeVector;
+}
+
+Elem& UVector::operator[ ](size_t index) // element at index
+{
+    return Array[index];
+}
+
+const Elem& UVector::operator[ ](size_t index) const // element at index
+{
+    return Array[index];
+}
+
+
+
+Iterator begin() const
+Iterator end() const // past the end position
+int UVector::Count() const
+Elem& At(size_t i); // element at index
+const Elem& At(size_t i) const; // element at index
+
+void Insert(size_t i, const Elem& e); // insert element at index
+void Reserve(size_t n); // reserve at least N spots
+
 
 
 
---
+++ Vector/Vector.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include <iostream>
+
+typedef int Elem;
+
+struct FNode
+{
+	FNode* Next;
+	FNode* Prev;
+	Elem Value;
+};
+
+class UVector
+{
+public:
+	class Iterator
+	{
+	public:
+		Elem& operator*()
+		{
+			return *Point;
+		}
+		bool operator==(const Iterator& p) const
+		{
+			return Point == p.Point;
+		}
+		bool operator!=(const Iterator& p) const
+		{
+			return Point != p.Point;
+		}
+		Iterator& operator++()
+		{
+			++Point;
+			return *this;
+		}
+		Iterator& operator−−()
+		{
+			--Point;
+			return *this;
+		}
+	private:
+		Elem* Point;
+	};
+public:
+	UVector();
+	UVector(const UVector& other);
+	UVector& operator=(const UVector& other);
+	~UVector();
+	bool Empty() const;
+	void Erase(size_t index);
+	Elem& operator[ ](size_t index);
+	const Elem& operator[ ](size_t index) const;
+	Iterator begin() const;
+	Iterator end() const;
+	int Count() const;
+	Elem& At(size_t i);
+	const Elem& At(size_t i) const;
+	void Insert(size_t i, const Elem& e);
+	void Reserve(size_t n);
+private:
+	FNode* Array;
+	size_t SizeVector{};
+	size_t Capacity{};
+};
